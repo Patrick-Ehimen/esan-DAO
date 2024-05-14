3f8860752ebd07ad1155d33be4120fc57b90f1c5
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-05-14 23:56:36 +0100

    Create GovToken.sol

---
+++ src/GovToken.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract GovToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+
+    // The following functions are overrides required by Solidity.
+
+    /**
+     * @notice Mints `amount` tokens to the address `to`
+     * @param to The address receiving the newly minted tokens
+     * @param amount The amount of tokens to mint
+     */
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+    /**
+     * @notice Hook that is called after any transfer of tokens.
+     * @param from The address tokens are transferred from
+     * @param to The address tokens are transferred to
+     * @param amount The amount of tokens transferred
+     */
+    function _afterTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    /**
+     * @notice Creates `amount` tokens and assigns them to `to`, increasing the total supply.
+     * @param to The address receiving the newly minted tokens
+     * @param amount The amount of tokens to mint
+     */
+    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._mint(to, amount);
+    }
+
+    /**
+     * @notice Destroys `amount` tokens from `account`, reducing the total supply.
+     * @param account The account whose tokens will be burned
+     * @param amount The amount of tokens to burn
+     */
+    function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._burn(account, amount);
+    }
+}
