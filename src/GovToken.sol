efbffe2ce91a01f94b6471ea63c2b71a9e273af9
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-05-15 09:55:05 +0100

    Update GovToken.sol

--- src/GovToken.sol
+++ src/GovToken.sol
@@ -5,8 +5,7 @@ pragma solidity ^0.8.19;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-
-contract GovToken is ERC20, ERC20Permit, ERC20Votes {
+contract GovToken is ERC20Permit, ERC20Votes {
     constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
 
     // The following functions are overrides required by Solidity.
@@ -47,4 +46,5 @@ contract GovToken is ERC20, ERC20Permit, ERC20Votes {
     function _burn(address account, uint256 amount) internal override(ERC20, ERC20Votes) {
         super._burn(account, amount);
     }
+
 }
